34b717a7836d5b9af1050b505424828c1623d1d5
Khakimov Doston <Khakimovmma12@gmail.com>
2025-01-02 17:29:41 +0500

    Task3

---
+++ Tyuiu.KhakimovDKh.Sprint2.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.KhakimovDKh.Sprint2.Task3.V1.Lib;
+namespace Tyuiu.KhakimovDKh.Sprint2.Task3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.KhakimovDKh.Sprint2.Task3.V1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.KhakimovDKh.Sprint2.Task3.V1.Lib;
+namespace Tyuiu.KhakimovDKh.Sprint2.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Хакимов Д.Х. | ИИПБ-24-2";
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Хакимов Достонбек Хисравович | ИИПБ-24-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(x);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+
+}
